60b8c13e57918608e86122fadbfbeb217b2a4545
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-03-06 16:21:03 -0800

    updated modules/recipes to have dietTags instead of itemTags

--- client/src/redux/modules/recipes.js
+++ client/src/redux/modules/recipes.js
@@ -1,13 +1,13 @@
 // ACTIONS
-const GET_ITEM_TAG = 'GET_ITEM_TAG';
+const GET_DIET_TAG = 'GET_DIET_TAG';
 
 // ACTION CREATORS
 // Actions are payloads of information that send data from your
 // application to your store. They are the only source of information
 // for the store. You send them to the store using store.dispatch().
 
-export const getItemTags = tag => ({
-  type: GET_ITEM_TAG,
+export const getDietTags = tag => ({
+  type: GET_DIET_TAG,
   payload: tag,
 });
 
@@ -15,13 +15,13 @@ export const getItemTags = tag => ({
 
 export default (
   state = {
-    itemTags: [],
+    dietTags: [],
   },
   action,
 ) => {
   switch (action.type) {
-    case GET_ITEM_TAG: {
-      return { ...state, itemTags: action.payload };
+    case GET_DIET_TAG: {
+      return { ...state, dietTags: action.payload };
     }
     default:
       return state;
